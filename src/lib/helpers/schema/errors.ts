b914d842c703aed1524ceb72c60b467427a91f9b
Alex Colby <alexanderdcolby@gmail.com>
2023-08-12 23:05:58 +0100

    feat: adds additional error for process function   failure

--- src/lib/helpers/schema/errors.ts
+++ src/lib/helpers/schema/errors.ts
@@ -1,15 +1,25 @@
-export type Result<Data, Error> = { data: Data } | { error: Error }
+export type Result<Data, Error> = { data?: Data; error?: Error }
 
 export class NotConnectedError extends Error {
   name = 'NotConnectedError'
   message = 'Input is not connected'
 }
 
-export class InputError {
+export class InputError extends Error {
   name = 'InputError'
   message
 
-  constructor(input_idx: number, public cause: Error) {
-    this.message = `Input ${input_idx} is an error`
+  constructor(public input_idx: number, public cause: Error) {
+    super()
+    this.message = `Error in input ${input_idx}`
+  }
+}
+
+export class ProcessError extends Error {
+  name = 'ProcessError'
+  message = 'Error in process function'
+
+  constructor(public cause: any) {
+    super()
   }
 }
