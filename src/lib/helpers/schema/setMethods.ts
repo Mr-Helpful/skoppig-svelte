3ee9740d3650c40004022e26cd46f44c9141e333
Alex Colby <alexanderdcolby@gmail.com>
2023-08-13 09:01:26 +0100

    feat: adds set unions to helpers

--- src/lib/helpers/schema/setMethods.ts
+++ src/lib/helpers/schema/setMethods.ts
@@ -1,9 +1,9 @@
-// export const union = <T>(set1: Set<T>, set2: Set<T>): Set<T> => {
-//   let set = new Set<T>()
-//   set1.forEach(x => set.add(x))
-//   set2.forEach(x => set.add(x))
-//   return set
-// }
+export const union = <T>(set1: Set<T>, set2: Set<T>): Set<T> => {
+  let set = new Set<T>()
+  set1.forEach(x => set.add(x))
+  set2.forEach(x => set.add(x))
+  return set
+}
 
 export const intersect = <T>(set1: Set<T>, set2: Set<T>): Set<T> => {
   let set = new Set<T>()
